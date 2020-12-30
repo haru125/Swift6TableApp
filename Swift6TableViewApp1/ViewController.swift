73bf15c1d465d94c66add3503b90f39e1ef6be1e
haru125 <jhonathan1471@gmail.com>
2020-12-31 06:43:20 +0900

    first commit

---
+++ Swift6TableViewApp1/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Swift6TableViewApp1
+//
+//  Created by 太田都寿 on 2020/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+    
+    var textArray = [String]()
+    var imageArray = ["1","2","3","4","5"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return textArray.count
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 724
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let imageView = cell.contentView.viewWithTag(1) as! UIImageView
+        let label = cell.contentView.viewWithTag(2) as! UILabel
+        
+        label.text = textArray[indexPath.row]
+        imageView.image = UIImage(named: imageArray[indexPath.row])
+        
+        return cell
+        
+        
+        
+    }
+
+    @IBAction func tap(_ sender: Any) {
+        
+        if textArray.count > 5 || textField.text?.isEmpty == true{
+            
+            return
+        }
+        
+        textArray.append(textField.text!)
+        textField.text = ""
+        tableView.reloadData()
+    }
+    
+}
+
